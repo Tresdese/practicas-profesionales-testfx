d4451dbdfe0f2f343d73a7a379215cd769bc8a7a
ByTheAngel11 <arangeldejesus@hotmail.com>
2025-05-24 01:27:33 -0600

    se modifico el pom

---
+++ src/gui/GUI_UploadReport.java
@@ -0,0 +1,15 @@
+package gui;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI_UploadReport extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
